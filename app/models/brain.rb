d5b457de923f6829bccafbe515a740db300bded8
Shahin Hojabrian <shahin70000@gmail.com>
2012-10-01 23:35:20 +0300

    zombie have one brain

--- app/models/brain.rb
+++ app/models/brain.rb
@@ -1,3 +1,4 @@
 class Brain < ActiveRecord::Base
   attr_accessible :flavor, :status, :zombie_id
+  belongs_to :zombie
 end
--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -1,14 +1,15 @@
 class Zombie < ActiveRecord::Base
-  attr_accessible :age, :bio, :name
+  has_one :brain, dependent: :destroy
+  attr_accessible :bio, :name
 
-  before_save :make_rotting
-  scope :rotting, where(rotting: true)
-  scope :fresh, where("age < 20")
-  scope :recent, order("created_at desc").limit(3)
 
-  def make_rotting
-      self.rotting = true if age > 20
-    end
-  end
+#  before_save :make_rotting
+#  scope :rotting, where(rotting: true)
+#  scope :fresh, where("age < 20")
+#  scope :recent, order("created_at desc").limit(3)
+
+#  def make_rotting
+#      self.rotting = true if age > 20
+#  end
 
 end
